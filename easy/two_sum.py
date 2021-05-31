46256251d63173722c2404100db73dc7ebe249d1
Hopko <codehopko@gmail.com>
2021-05-31 18:44:43 -0500

    adding binary search

--- easy/two_sum.py
+++ easy/two_sum.py
@@ -1,43 +1,70 @@
 from collections import defaultdict
+from math import floor, ceil
+
 
 def twoSum(self, nums: List[int], target: int) -> List[int]:
-    """Searches a list of numbers for two values whose sum equals a target value"""
+    '''Searches a list of numbers for two values whose sum equals a target value'''
 
-    # Brute force 
-    # Time: O(n^2)    Space: 1    
+    # Brute force
+    # Time: O(n^2)    Space: 1
     for i, num in enumerate(nums):
         if (target - num) in nums[i+1:]:
             return i, nums[i+1:].index(target-num) + i + 1
 
     # Brute force #2 (less pythonic)
-    # Time: O(n^2)    Space: 1    
+    # Time: O(n^2)    Space: 1
     for i, num1 in enumerate(nums):
         for j, num2 in enumerate(nums[i+1:]):
             if num1+num2 == target:
                 return i, (j+i+1)
-    
+
     # Dictionary
     # Time: O(n)    Space: O(n)
     num_dict = defaultdict(list)
 
     for i, num in enumerate(nums):
         num_dict[num].append(i)
-    
+
     for num, index in num_dict.items():
         # for cases where (target - num) == num
-        if num*2 == target: 
+        if num*2 == target:
             if len(index) > 1:
                 return index[:2]
         elif (target - num) in num_dict:
             return index[0], num_dict[target - num][0]
 
-
     # Sort then Binary search
-    # Time: O(nlog(n))  Space: 1
-    # nums.sort()
+    # Time: O(nlog(n))  Space: log(n)?
+    for i, num in enumerate(nums):
+        other_num = target - num
+        other_num_idx = binary_search(
+            nums, i+1, len(nums)-1, other_num, ordered=False)
 
-    # for i, num in enumerate(nums):
-    #     if target - num < num
+        if other_num_idx:
+            return i, other_num_idx
 
-    
-        
+    return None
+
+
+def binary_search(arr, start, end, target, ordered=True):
+    '''uses binary search to locate a target number'''
+    if not ordered:
+        arr.sort()
+        binary_search(arr, start, end, target)
+
+    while end - start >= 1:
+        mid_idx = floor((end - start)/2)
+        mid_val = arr[mid_idx]
+
+        if mid_val == target:
+            return mid_idx
+
+        if mid_val > target:
+            # search left
+            binary_search(arr, start, mid_idx-1, target)
+
+        if mid_val < target:
+            # search right
+            binary_search(arr, mid_idx+1, end, target)
+
+    return None
